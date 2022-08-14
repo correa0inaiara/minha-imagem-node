438ed6b5fadb32223b7c425b610d27485dd84218
Naiara Correa <correa0inaiara@gmail.com>
2022-08-14 00:25:30 +0000

    Add script bash, banco de dados MYSQL e alteracoes na pasta do projeto

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+    host: 'db', // nome do serviço, através de rede compartilada pelo docker
+    user: 'root',
+    password: 'root',
+    database: 'mydb'
+})
+
+connection.connect()
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World com node.js dentro do docker!')
+    res.send('\nHello from My Node Image!!!\n\n+ Com user definido no Dockerfile, por questões de segurança.\n+ Docker Compose: para orquestração dos containers.\n+ Script bash para execução dos comandos dentro do container, evitando ter que rodá-los manualmente no terminal.\n+ Conexão com MYSQL em outro container.')
 })
 
 app.listen(process.env.PORT || 3000, () => {
